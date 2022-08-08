51fe540f3fbdec316309ddfcb5298523c1791302
Slav <shutrow@rambler.ru>
2022-08-08 13:21:31 +0300

    01-Counter RootComponent added

---
+++ 01-basics/01-CounterButtonApp/RootComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Counter</h1>
+  <button type="button" @click="add">{{ counter }}</button>
+</template>
+
+<script>
+export default {
+  name: 'counter',
+  data: () => ({
+    counter: 0,
+  }),
+  methods: {
+    add() {
+      this.counter++
+    },
+  },
+}
+</script>
