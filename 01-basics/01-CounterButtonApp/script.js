6042adfbb89c7ccb199b23a0f36711d34f10dbb1
Slav <shutrow@rambler.ru>
2022-08-08 13:12:01 +0300

    01-Counter done

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,6 @@
 // import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from 'vue'
+import RootComponent from './RootComponent'
 
 // Создайте Vue приложение
+createApp(RootComponent).mount('#app')
