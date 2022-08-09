57adc27055ae139c0c49aafb98adb7fc526eb847
Slav <shutrow@rambler.ru>
2022-08-09 09:28:17 +0300

    01-basics done

---
+++ 01-basics/03-MarkedList/RootComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="form-group">
+      <input type="search" v-model="pattern" />
+    </div>
+    <ul v-for="email of selectedEmails">
+      <li :class="email[1] ? 'marked' : ''">{{ email[0] }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { emails } from './data'
+
+export default {
+  name: 'markedList',
+  data: () => ({
+    pattern: '',
+    selected: false,
+  }),
+  computed: {
+    selectedEmails() {
+      return emails.map((email) => [email, (this.selected = email.includes(this.pattern) && this.pattern)])
+    },
+  },
+}
+</script>
---
+++ 01-basics/04-MeetupTitle/RootComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <label v-for="n in 5"><input type="radio" name="meetupId" :value="n" v-model="meetupId" />{{ n }}</label>
+
+    <hr />
+    <h3>{{ meetupTitle }}</h3>
+  </div>
+</template>
+
+<script>
+const API_URL = 'https://course-vue.javascript.ru/api'
+
+function fetchMeetupById(meetupId) {
+  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      return response.json().then((error) => {
+        throw error
+      })
+    }
+  })
+}
+
+export default {
+  name: 'meetUpTitle',
+  data: () => ({
+    meetupId: 0,
+    meetupTitle: '',
+  }),
+  watch: {
+    async meetupId(value) {
+      try {
+        const meetupData = await fetchMeetupById(value)
+        this.meetupTitle = meetupData.title
+      } catch (err) {
+        console.log(err.message)
+      }
+    },
+  },
+}
+</script>
