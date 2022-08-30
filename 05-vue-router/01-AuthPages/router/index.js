f4a7e532f89fda488030a4f27a899a3f8bc49edc
Slav <shutrow@rambler.ru>
2022-08-30 08:10:25 +0300

    05-01 AuthPages done

--- 05-vue-router/01-AuthPages/router/index.js
+++ 05-vue-router/01-AuthPages/router/index.js
@@ -1,12 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { PageIndex } from '@views/PageIndex'
+import PageIndex from '../views/PageIndex'
+import PageLogin from '../views/PageLogin'
+import PageRegister from '../views/PageRegister'
 
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/01-AuthPages'),
   routes: [
     {
       path: '/',
+      name: 'home',
       component: PageIndex,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: PageLogin,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: PageRegister,
     },
   ],
 })
